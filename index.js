b20b1273073542f9291cbfc4e12e27cc0c633668
Matt Filion <matt.filion@gmail.com>
2017-04-28 00:45:08 -0700

    Fix bug with updating a config.

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ function Config(_config){
 
   const config   = _config ? _config : {};
   const prefix   = config.envPrefix ? config.envPrefix : '';
-  const defaults = config.defaults && Object.keys(config.defaults).length !== 0 ? config.defaults : null;
+  const defaults = config.defaults && Object.keys(config.defaults).length !== 0 ? config.defaults : {};
 
   const exists       = value => value !== undefined && value !== null;
   const isObject     = value => typeof value === 'object';
-  const lookForValue = (path,parent) => parent ? path.split('.').reduce( (accumulator,name) => accumulator[name], parent) : null;
+  const lookForValue = path => path.split('.').reduce( (accumulator,name) => accumulator[name], defaults);
   const lookInEnv    = name => process.env[`${prefix}${name.replace(/([^a-zA-Z0-9_])/g,'_')}`];
   const get          = (name,inlineDefault) => {
     let value = lookInEnv(name)
-    if(!exists(value)) value = lookForValue(name,defaults)
+    if(!exists(value)) value = lookForValue(name)
     if(!exists(value)) value = inlineDefault
     return value;
   };
-  const deepCopy       = (target,source) => Object.keys(source).forEach( key => target[key] && typeof target[key] === 'object' ? deepCopy(target[key],source[key]) : target[key] = source[key] )
+  const deepCopy       = (target,source) => Object.keys(source).forEach( key => target[key] && typeof target[key] === 'object' ? deepCopy(target[key],source[key]) : target[key] = source[key] );
   const objEnvOverride = (name,value) => {
     if(isObject(value)){
       Object.keys(value).forEach( key => {
@@ -32,7 +32,7 @@ function Config(_config){
 
   const instance = {
     get: (name,inlineDefault) => objEnvOverride( name, get(name,inlineDefault) ),
-    update: configs => deepCopy(defaults || {}, configs),
+    update: configs => deepCopy(defaults, configs),
     childOf: name => new Config({envPrefix:`${prefix}${prefix ? '_' : ''}${name}`,defaults:instance.get(name) })
   }
 
